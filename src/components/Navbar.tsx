081a2cfa4ebce90b8d375028fe9f08840916d3e3
adamaconguli <waltson2003@gmail.com>
2023-11-20 21:21:16 +0800

    responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,31 @@
 import { NavLink } from 'react-router-dom'
 import '../assets/Navbar.css'
+import { useRef } from 'react'
 
 export default function Navbar() {
+  const menu = useRef<HTMLDivElement | null>(null)
+  const overlay = useRef<HTMLDivElement | null>(null)
+
+  const toggleMenu = () => {
+    const display = menu.current?.style.display
+    if (display === 'none' && menu.current && overlay.current) {
+      menu.current.style.display = 'grid'
+      overlay.current.style.display = 'block'
+      overlay.current.style.display
+    } else if (menu.current && overlay.current) {
+      menu.current.style.display = 'none'
+      overlay.current.style.display = 'none'
+    }
+  }
+
   return (
     <nav className='Navbar'>
       <div className='Navbar-logo'>
         <i className='bx bxl-typescript'></i>
       </div>
-      <div className='Navbar-list'>
+      <i className='bx bx-menu' onClick={toggleMenu}></i>
+      <div className='Navbar-overlay' onClick={toggleMenu} ref={overlay}></div>
+      <div className='Navbar-list' ref={menu} onClick={toggleMenu}>
         <NavLink to='/' className={({ isActive }) => (isActive ? 'Navbar-current' : '')}>
           Todo
         </NavLink>
